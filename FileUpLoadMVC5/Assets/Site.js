413f7cd64bb7a844ae3f3ec38b4a0534aff24486
uzair <uzairanwar2299@gmail.com>
2021-04-19 23:01:16 +0500

    Add project files.

---
+++ FileUpLoadMVC5/Assets/Site.js
@@ -0,0 +1,49 @@
+﻿function showImage(event) {
+    //--------------------------------------under testing script--------------------------------------------
+    var filename = $('#filePreview').val();
+    if (filename.substring(3, 11) == 'fakepath') {
+        filename = filename.substring(12);
+    }
+    $('#my_file').html(filename);
+
+    //-----------------------------------------------------------------------------------------------
+    console.log("hello");
+    //--------------------------------------tested script--------------------------------------------
+    if (event.target.files.length > 0) {
+        var file = event.target.files[0].name;
+        var scr = URL.createObjectURL(event.target.files[0]);
+        var preview = document.getElementById("ImagePreview");
+        preview.src = scr;
+        preview.style.display = "block";
+        //document.getElementById("ImagePreviewLabel").innerText = name;
+    }
+    //-----------------------------------------------------------------------------------------
+}
+
+//----------------------------------------------Tested----------------------------------------------------
+function readURL(input) {
+    if (input.files && input.files[0]) {
+        var reader = new FileReader();
+        reader.onload = function (e) {
+            $('#ImagePreview').attr('src', e.target.result);
+        }
+        var preview = document.getElementById("ImagePreview");
+        preview.style.display = "block";
+        reader.readAsDataURL(input.files[0]); // convert to base64 string
+    }
+}
+//---------------------------------------------------------------------------------------------------------
+//-----------------------------------------------Tested----------------------------------------------------
+function readURLMultiple(input) {
+    if (input.files) {
+        var filesAmount = input.files.length;
+        for (i = 0; i < filesAmount; i++) {
+            var reader = new FileReader();
+            reader.onload = function (event) {
+                $($.parseHTML('<img>')).attr('src', event.target.result).appendTo('#gallery');
+            }
+            reader.readAsDataURL(input.files[i]);
+        }
+    }
+}
+//-----------------------------------------------------------------------------------------------------------
---
+++ FileUpLoadMVC5/Controllers/HomeController.cs
@@ -0,0 +1,90 @@
+﻿using FileUpLoadMVC5.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FileUpLoadMVC5.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+
+             [HttpGet]
+        public ActionResult PreviewMultipleFile()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult FilePreviewBeforeUpload()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult FileUpload()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult FileUpload(User user, HttpPostedFileBase file)
+        {
+            if (file == null){
+                //in the case file is null
+            }
+            else
+            {
+                string path = Server.MapPath("~/Images");
+                string fileName = System.IO.Path.GetFileNameWithoutExtension(file.FileName);
+                string fileExtension = System.IO.Path.GetExtension(file.FileName);
+                fileName = Guid.NewGuid() + fileExtension;
+                //string fileName = System.IO.Path.GetFileName(file.FileName);
+                string fullPath = System.IO.Path.Combine(path, fileName);
+                file.SaveAs(fullPath);
+            }
+            return View();
+        }
+
+        public FileResult FileDownload(User user, HttpPostedFileBase file)
+        {
+                string path = Server.MapPath("~/Images");
+                string fileName = System.IO.Path.GetFileName("a025c4b7-fd28-459d-b417-28a9332e2563.jpg");
+                //string fileExtension = System.IO.Path.GetExtension(file.FileName);
+                //fileName = Guid.NewGuid() + fileExtension;
+                //string fileName = System.IO.Path.GetFileName(file.FileName);
+                string fullPath = System.IO.Path.Combine(path, fileName);
+            return File(fullPath,"image/*","download.jpg");
+        }
+
+        public ActionResult FileUploadAjax()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult FileUploadAjax(User user)
+        {
+            return View();
+        }
+    }
+}
